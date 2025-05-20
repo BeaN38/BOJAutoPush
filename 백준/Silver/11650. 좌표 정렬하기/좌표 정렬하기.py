acbfae5d42401ee880f9e65fcee5cc29bf7ceb22
BeaN38 <freshbean038@gmail.com>
2025-05-20 14:45:10 +0900

    Update 좌표 정렬하기.py

--- 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.py
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.py
@@ -1,10 +1,10 @@
 test_case = int(input()) # 테스트 케이스 입력
-xy = [] # 좌표 저장 리스트 생성성
+xy = [] # 좌표 저장 리스트 생성
 
-for i in range(test_case): # 테스트 케이스만큼 좌표를 입력받아서 xy 리스트에 저장장
+for i in range(test_case): # 테스트 케이스만큼 좌표를 입력받아서 xy 리스트에 저장
     xy.append(list(map(int, input().split())))
 
-xy.sort() # 파이썬에서 2차원 배열을 sort하면 첫번째 인자의 크기를 비교한 후, 두번쨰 인자의 크기를 비교하여 정렬함함
+xy.sort() # 파이썬에서 2차원 배열을 sort하면 첫번째 인자의 크기를 비교한 후, 두번쨰 인자의 크기를 비교하여 정렬함
 
-for i in range(test_case): # 답 출력력
+for i in range(test_case): # 답 출력
     print(str(xy[i][0]) + " " + str(xy[i][1]))
